b09e8e3b0d1b79d838497a11a77da414078a8c4d
Ваше Имя <you@example.com>
2022-08-23 22:03:54 +0300

    "Вт 23 авг 2022 22:03:54" -

--- core/core_parsing.py
+++ core/core_parsing.py
@@ -56,8 +56,6 @@ class ParsingBase:
         # TODO: Добавить тесты для ImageMd
         res = CoreMarkdown.ImgMd(res)
         # TODO: Реализовать вспомогательный текст (символ `> ` в начале строки)
-
-        # TODO: Вставить тег <br> если есть два переноса
         res = CoreMarkdown.Br(res)
         return res
 
